ca9ec37b8123b88793913f25ae09acf4ed25452b
machadomrm <85651380+machadomrm@users.noreply.github.com>
2021-11-26 09:15:23 -0300

    ATUALIZAR

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,17 @@
 const Api = {
-  apiUrl: 'http://localhost:3001/vagas',
-  fetchGetAll: () => fetch(Api.apiUrl),
-  fetchGetById: id => fetch(`${Api.apiUrl}/${id}`),
-  fetchPost: (vaga) => {
-    return fetch(`${Api.apiUrl}/add`, {
+  apiUrl: '  https://git.heroku.com/tripulacoes',
+    fetchGetAll: () => fetch(`${Api.apiUrl}/readAll`),
+    fetchGetById: id => fetch(`${Api.apiUrl}/readSingle/${id}`),
+    fetchPost: (vaga) => {
+    return fetch(`${Api.apiUrl}/create`, {
       method: 'POST',
       headers: new Headers({
         "Content-Type": "application/json",
       }),
       body: JSON.stringify(vaga)
-    })
+    });
   },
+
   fetchPut: (vaga, id) => {
     return fetch(`${Api.apiUrl}/update/${id}`, {
       method: 'PUT',
@@ -18,13 +19,13 @@ const Api = {
         "Content-Type": "application/json",
       }),
       body: JSON.stringify(vaga)
-    })
+    });
   },
   fetchDelete: (id) => {
     return fetch(`${Api.apiUrl}/delete/${id}`, {
       method: 'DELETE'
-    })
+    });
   }
-}
+};
 
 export default Api;
